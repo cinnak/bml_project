2ff5edbbfd52129505f8eda4b004c987e73be256
cinnak <121988836+cinnak@users.noreply.github.com>
2024-05-17 03:07:35 +0000

    链接所有博客到ndexy页面

--- blogmaker_lite.py
+++ blogmaker_lite.py
@@ -1,22 +1,27 @@
 from pathlib import Path
-from django.contrib import admin
+
 from django.urls import path
 from django.core.handlers.wsgi import WSGIHandler
 from django.shortcuts import render
 from django.contrib import admin
 
-
 from blogs.models import Blog, BlogPost
+
+
 admin.site.register((Blog, BlogPost))
 
 def index(request):
     return render(request, "index.html")
 
+def blogs(request):
+    all_blogs = Blog.objects.all()
+    context = {"blogs": all_blogs}
+    return render(request, "blogs.html", context)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index)
+    path("blogs/", blogs, name="blogs"),
+    path("", index, name="index"),
 ]
 
 application = WSGIHandler()
-for x in admin.site.urls:
-    print(x)
